16cfed0091e282c36a8a5906a4a1cc2e68d5400a
mrkau99 <mrkaumohanty@gmail.com>
2021-05-23 22:30:37 +0530

    Complete Todo App

---
+++ src/Components/Form.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../App.css";
+
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
+
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        className="todo-input"
+        value={inputText}
+        onChange={inputTextHandler}
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="incomplete">Incomplete</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
